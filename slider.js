87d31327b701ed1a47ad82995a83d45a81822fe9
Clément Creusat <clement.creusat@gmail.com>
2021-06-18 10:31:30 +0200

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,42 +1,43 @@
 function Slider() {
 	"use strict";
 	// Get Triggers
-	const prev = document.getElementById('prev');
-	const next = document.getElementById('next');
+	const prev = document.getElementById("prev");
+	const next = document.getElementById("next");
 	// Touches Triggers
 	let touchStartX = 0;
 	let touchEndX = 0;
 	// Set all variables related to the slides component
-	const slideWrapper = document.getElementById('slider-image');
-	const slides = document.querySelectorAll('.slide');
+	const slideWrapper = document.getElementById("slider-image");
+	const slides = document.querySelectorAll(".slide");
 	let slidesLength = slides.length;
 	const currentSlide = slides[0];
 	const lastSlide = slides[slidesLength - 1];
 	const cloneFirstSlide = currentSlide.cloneNode(true);
 	const cloneLastSlide = lastSlide.cloneNode(true);
 	// Set all variables related to the content component
-	const contentWrapper = document.getElementById('slider-content');
-	const contents = document.querySelectorAll('.content');
+	const contentWrapper = document.getElementById("slider-content");
+	const contents = document.querySelectorAll(".content");
 	let contentsLength = contents.length;
 	const currentContent = contents[0];
 	const lastContent = contents[contentsLength - 1];
 	const cloneFirstContent = currentContent.cloneNode(true);
 	const cloneLastContent = lastContent.cloneNode(true);
 	// Media Query
-	const mediaQueryMAX = window.matchMedia('(max-width: 50em)');
-	const mediaQueryMIN = window.matchMedia('(min-width: 50em)');
+	const mediaQueryMAX = window.matchMedia("(max-width: 50em)");
+	const mediaQueryMIN = window.matchMedia("(min-width: 50em)");
 	// Set Index to 1
 	let index = 1;
 
 	function matchMedia() {
 		// Check if the media query is true
 		if (mediaQueryMAX.matches) {
-			document.querySelector('.slider-content').style.maxHeight = currentContent.clientHeight + "px";
-			document.querySelector('.slider-content').style.overflow = "hidden";
+			document.querySelector(".slider-content").style.maxHeight =
+				currentContent.clientHeight + "px";
+			document.querySelector(".slider-content").style.overflow = "hidden";
 		}
-		
+
 		if (mediaQueryMIN.matches) {
-			document.querySelector('.slider-content').removeAttribute("style");
+			document.querySelector(".slider-content").removeAttribute("style");
 		}
 	}
 
@@ -50,9 +51,10 @@ function Slider() {
 	}
 
 	function onResize() {
-		window.addEventListener('resize', () => {
+		window.addEventListener("resize", () => {
 			slideWrapper.style.left = -currentSlide.offsetWidth * index + "px";
-			contentWrapper.style.top = -currentContent.clientHeight * index + "px";
+			contentWrapper.style.top =
+				-currentContent.clientHeight * index + "px";
 
 			matchMedia();
 		});
@@ -64,9 +66,9 @@ function Slider() {
 	}
 
 	function moveSlide(direction) {
-		slidesLength = document.querySelectorAll('.slide').length;
-		slideWrapper.classList.add('transitionLeft');
-		contentWrapper.classList.add('transitionTop');
+		slidesLength = document.querySelectorAll(".slide").length;
+		slideWrapper.classList.add("transitionLeft");
+		contentWrapper.classList.add("transitionTop");
 
 		if (direction === -1) {
 			index--;
@@ -75,8 +77,10 @@ function Slider() {
 			if (index === 0) {
 				index = 3;
 				slideWrapper.addEventListener("transitionend", () => {
-					slideWrapper.style.left = -currentSlide.offsetWidth * index + "px";
-					contentWrapper.style.top = -currentContent.clientHeight * index + "px";
+					slideWrapper.style.left =
+						-currentSlide.offsetWidth * index + "px";
+					contentWrapper.style.top =
+						-currentContent.clientHeight * index + "px";
 				});
 			}
 		}
@@ -84,18 +88,20 @@ function Slider() {
 			index++;
 			changeSlide();
 
-			if (index === slidesLength -1) {
+			if (index === slidesLength - 1) {
 				index = 1;
 				slideWrapper.addEventListener("transitionend", () => {
-					slideWrapper.style.left = -currentSlide.offsetWidth * index + "px";
-					contentWrapper.style.top = -currentContent.clientHeight * index + "px";
+					slideWrapper.style.left =
+						-currentSlide.offsetWidth * index + "px";
+					contentWrapper.style.top =
+						-currentContent.clientHeight * index + "px";
 				});
 			}
 		}
 
 		slideWrapper.addEventListener("transitionend", () => {
-			slideWrapper.classList.remove('transitionLeft');
-			contentWrapper.classList.remove('transitionTop');
+			slideWrapper.classList.remove("transitionLeft");
+			contentWrapper.classList.remove("transitionTop");
 		});
 	}
 
@@ -113,26 +119,26 @@ function Slider() {
 
 	function handleGesture() {
 		if (touchEndX < touchStartX) {
-			moveSlide(1)
+			moveSlide(1);
 		}
 		if (touchEndX > touchStartX) {
-			moveSlide(-1)
+			moveSlide(-1);
 		}
 	}
 
 	// Events
 	/// Click Events
-	prev.addEventListener('click', () => moveSlide(-1));
-	next.addEventListener('click', () => moveSlide(1));
+	prev.addEventListener("click", () => moveSlide(-1));
+	next.addEventListener("click", () => moveSlide(1));
 	/// Key Event
-	document.addEventListener('keydown', (e) => {
+	document.addEventListener("keydown", e => {
 		if (e.key === "ArrowLeft") moveSlide(-1);
 		if (e.key === "ArrowRight") moveSlide(1);
 	});
 	/// Touch Events
-	slideWrapper.addEventListener('touchstart', dragStart);
-	slideWrapper.addEventListener('touchmove', dragAction);
-	slideWrapper.addEventListener('touchend', dragEnd);
+	slideWrapper.addEventListener("touchstart", dragStart);
+	slideWrapper.addEventListener("touchmove", dragAction);
+	slideWrapper.addEventListener("touchend", dragEnd);
 
 	addClones();
 	matchMedia();
